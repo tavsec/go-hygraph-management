5fc2131cc1d3c63f78ed9d87be062ea4ef38fea1
Timotej Avsec <avsectimotej1@gmail.com>
2023-02-25 06:58:10 +0100

    Add Hygraph client

---
+++ client.go
@@ -0,0 +1,33 @@
+package go_hygraph_management
+
+import (
+	"context"
+	"github.com/machinebox/graphql"
+)
+
+type Client struct {
+	HostURL       string
+	GraphQlClient *graphql.Client
+	AuthToken     string
+}
+
+func NewClient(host, authToken string) (*Client, error) {
+	c := Client{
+		GraphQlClient: graphql.NewClient(host),
+		HostURL:       host,
+	}
+
+	c.AuthToken = authToken
+
+	return &c, nil
+}
+
+func (c *Client) MakeRequest(ctx context.Context, query string, variables map[string]string, responseData interface{}) error {
+	req := graphql.NewRequest(query)
+	req.Header.Add("Authorization", "Bearer "+c.AuthToken)
+	for key, value := range variables {
+		req.Var(key, value)
+	}
+
+	return c.GraphQlClient.Run(ctx, req, &responseData)
+}
