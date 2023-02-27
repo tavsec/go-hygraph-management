80bb2eb715b302b302e0842850f673b121697ebc
Timotej Avsec <avsectimotej1@gmail.com>
2023-02-27 19:01:47 +0100

    Add gock mock

--- client_test.go
+++ client_test.go
@@ -1,25 +1,43 @@
 package go_hygraph_management
 
 import (
-	"os"
+	"context"
+	"github.com/h2non/gock"
+	"github.com/nbio/st"
 	"testing"
 )
 
 func TestNewClient(t *testing.T) {
-	client, err := NewClient("https://management.hygraph.com/graphql", os.Getenv("HYGRAPH_AUTH_TOKEN"))
+	client, err := NewClient("https://management.hygraph.com/graphql", "auth_token")
 	if err != nil {
 		t.Error(err)
 	}
 
-	if client.HostURL != "https://management.hygraph.com/graphql" {
-		t.Error("HostURL not set correctly")
-	}
-
-	if client.AuthToken != os.Getenv("HYGRAPH_AUTH_TOKEN") {
-		t.Error("AuthToken not set correctly")
-	}
+	st.Expect(t, client.HostURL, "https://management.hygraph.com/graphql")
+	st.Expect(t, client.AuthToken, "auth_token")
 
 	if client.GraphQlClient == nil {
 		t.Error("GraphQlClient not set correctly")
 	}
+}
+
+func TestClient_MakeRequest(t *testing.T) {
+	defer gock.Off()
+
+	gock.New("https://management.hygraph.com").
+		Post("/graphql").
+		Reply(200).
+		JSON(map[string]map[string]string{"data": {"foo": "bar"}})
+
+	client, _ := NewClient("https://management.hygraph.com/graphql", "auth_token")
+
+	var responseData struct {
+		Foo string `json:"foo"`
+	}
+	err := client.MakeRequest(context.Background(), "", map[string]string{"test": "arg"}, &responseData)
+
+	st.Expect(t, err, nil)
+	st.Expect(t, responseData.Foo, "bar")
+	st.Expect(t, gock.IsDone(), true)
+
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module tavsec/go-hygraph-management
 
 go 1.19
 
-require github.com/machinebox/graphql v0.2.2
+require (
+	github.com/h2non/gock v1.2.0
+	github.com/machinebox/graphql v0.2.2
+	github.com/nbio/st v0.0.0-20140626010706-e9e8d9816f32
+)
 
 require (
+	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
 	github.com/matryer/is v1.4.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 )
